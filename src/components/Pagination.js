48532da95934e962e805f30f3c663dfc42621393
sonipranjal <pranjalsoni6256@gmail.com>
2021-02-17 02:23:47 +0530

    minor bug netlify

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,4 +1,9 @@
 import React from "react";
+import styled from "styled-components";
+
+const Span = styled.span`
+  cursor: pointer;
+`;
 
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   const pageNumbers = [];
@@ -12,9 +17,9 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
       <ul className="pagination">
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} href="!#" className="page-link">
+            <Span onClick={() => paginate(number)} className="page-link">
               {number}
-            </a>
+            </Span>
           </li>
         ))}
       </ul>
